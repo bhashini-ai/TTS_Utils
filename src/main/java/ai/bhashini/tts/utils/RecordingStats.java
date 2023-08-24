284ed787846a7259e49594e123ee27fe8ba48e1e
Shiva Kumar H R <shivahr@gmail.com>
2023-08-24 19:58:53 +0530

    Moved from VaakSangraahakDesktop

---
+++ src/main/java/ai/bhashini/tts/utils/RecordingStats.java
@@ -0,0 +1,107 @@
+package ai.bhashini.tts.utils;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RecordingStats {
+	Duration total = Duration.ofSeconds(0);
+	HashMap<String, Duration> wavFilesAndDurations = new HashMap<String, Duration>();
+
+	public void add(String wavPath, boolean modified) {
+		Duration duration = getAudioDuration(wavPath, modified);
+		if (wavFilesAndDurations.containsKey(wavPath)) {
+			total = total.minus(wavFilesAndDurations.get(wavPath));
+		}
+		wavFilesAndDurations.put(wavPath, duration);
+		total = total.plus(duration);
+	}
+
+	public void remove(String wavPath) {
+		if (wavFilesAndDurations.containsKey(wavPath)) {
+			total = total.minus(wavFilesAndDurations.get(wavPath));
+			wavFilesAndDurations.remove(wavPath);
+		}
+	}
+
+	public int getNumRecordings() {
+		return wavFilesAndDurations.keySet().size();
+	}
+
+	private Duration getAudioDuration(String wavPath, boolean modified) {
+		File wavFile = new File(wavPath);
+		File normalizedWavFile = new File(wavFile.getParent() + "_normalized", wavFile.getName());
+		if (!normalizedWavFile.exists() || modified) {
+			TrimAndNormalizeAudio.trim(wavPath, normalizedWavFile.getAbsolutePath());
+		}
+		double secs = MeasureAudioLength.getAudioLengthInSecs(normalizedWavFile.getAbsolutePath(), null);
+		return Duration.ofMillis(Math.round(secs * 1000));
+	}
+
+	@Override
+	public String toString() {
+		return "#Sentences=" + wavFilesAndDurations.keySet().size() + "\tDuration=" + totalDurationAsString();
+	}
+
+	public String totalDurationAsString() {
+		long s = total.toSeconds();
+		return String.format("%02d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
+	}
+
+	public void loadStats(String wavDir) {
+		total = Duration.ofSeconds(0);
+		wavFilesAndDurations.clear();
+		File[] wavFiles = new File(wavDir).listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".wav");
+			}
+		});
+		if (wavFiles != null) {
+			for (File wavFile : wavFiles) {
+				add(wavFile.getAbsolutePath(), false);
+			}
+		}
+	}
+
+	public void add(RecordingStats recordingStats) {
+		for (String wavPath : recordingStats.wavFilesAndDurations.keySet()) {
+			Duration duration = recordingStats.wavFilesAndDurations.get(wavPath);
+			if (wavFilesAndDurations.containsKey(wavPath)) {
+				total = total.minus(wavFilesAndDurations.get(wavPath));
+			}
+			wavFilesAndDurations.put(wavPath, duration);
+			total = total.plus(duration);
+		}
+	}
+
+	public static void main(String[] args) {
+		File dataDir = new File(args[0]);
+		RecordingStats totalRecordingStats = new RecordingStats();
+		File[] subDirs = dataDir.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		});
+		if (subDirs == null) {
+			return;
+		}
+		Arrays.sort(subDirs);
+		System.out.println("WavDir:\t#Sentences\tDuration");
+		for (File subDir : subDirs) {
+			File wavDir = new File(subDir, "wav");
+			if (wavDir.exists()) {
+				RecordingStats recordingStats = new RecordingStats();
+				recordingStats.loadStats(wavDir.getAbsolutePath());
+				System.out.println(wavDir.getAbsolutePath() + ":\t" + recordingStats.getNumRecordings() + "\t"
+						+ recordingStats.totalDurationAsString());
+				totalRecordingStats.add(recordingStats);
+			}
+		}
+		System.out.println("\nTotal: " + totalRecordingStats.toString());
+	}
+}
