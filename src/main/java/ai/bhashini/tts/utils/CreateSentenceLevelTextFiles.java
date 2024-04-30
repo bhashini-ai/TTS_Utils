4e1e36fd3f65df23464aa7bac839cdb5ae173439
Shiva Kumar H R <shivahr@gmail.com>
2024-04-30 17:21:18 +0530

    Simplify code to leverage the unique sentenceIds in the scripts file located inside the script sub-directory.

--- src/main/java/ai/bhashini/tts/utils/CreateSentenceLevelTextFiles.java
+++ src/main/java/ai/bhashini/tts/utils/CreateSentenceLevelTextFiles.java
@@ -4,22 +4,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class CreateSentenceLevelTextFiles {
 
 	public static void main(String[] args) {
-		String scriptsDir = args[0];
-		HashMap<String, HashMap<Integer, String>> categoryWiseSentences = new HashMap<String, HashMap<Integer, String>>();
-		String[] categories = new String[] { "agri", "education", "finance", "general", "health", "other", "politics",
-				"weather", "books", "iitm" };
+		String recordingsDir = args[0];
+		File scriptsDir = new File(recordingsDir, "script");
+		String scriptSuffix = ".txt";
+		ArrayList<String> categories = getScriptCategories(scriptsDir, scriptSuffix);
+		HashMap<String, String> sentences = new HashMap<>();
 		for (String category : categories) {
-			HashMap<Integer, String> sentences = loadScript(
-					new File(scriptsDir, category + "_richSentences_validated.txt"));
-			categoryWiseSentences.put(category, sentences);
+			sentences.putAll(loadScript(new File(scriptsDir, category + scriptSuffix)));
 		}
-		String recordingsDir = args[1];
 		File[] subDirs = MatchWavAndTextFiles.getSubDirs(new File(recordingsDir));
 		for (File subDir : subDirs) {
 			File wavDir = new File(subDir, "wav");
@@ -28,52 +29,59 @@ public class CreateSentenceLevelTextFiles {
 			}
 			File txtDir = new File(subDir, "txt");
 			txtDir.mkdirs();
-			
+
 			File[] wavFiles = FileUtils.getWavFiles(wavDir);
 			for (File wavFile : wavFiles) {
-				String fileName = FileUtils.getFileNameWithoutExtension(wavFile, "wav");
-				int index2 = fileName.lastIndexOf('_');
-				try {
-					int sentenceNumber = Integer.parseInt(fileName.substring(index2 + 1));
-					String subFileName = fileName.substring(0, index2);
-					String category = subFileName.substring(subFileName.lastIndexOf('_') + 1).toLowerCase();
-					HashMap<Integer, String> sentences = categoryWiseSentences.get(category);
-					if (sentences == null) {
-						System.out.println("Couldn't find text for category: " + category);
-						continue;
-					}
-					String text = sentences.get(sentenceNumber);
-					if (text == null) {
-						System.out.println("Couldn't find text for " + wavFile.getAbsolutePath());
-						continue;
-					}
-					String textFilePath = new File(txtDir, wavFile.getName().replace(".wav", ".txt")).getAbsolutePath();
-					if (new File(textFilePath).exists()) {
-						// System.out.println("Skipping already existing file " + textFilePath);
-						continue;
-					}
-					System.out.println("Creating " + textFilePath);
-					FileUtils.createFileWithContents(textFilePath, text);
-				} catch (NumberFormatException e) {
-					System.out.println("Error parsing filename of " + wavFile.getAbsolutePath());
+				String sentenceId = FileUtils.getFileNameWithoutExtension(wavFile, "wav");
+				String text = sentences.get(sentenceId);
+				if (text == null) {
+					System.out.println("Couldn't find text for " + wavFile.getAbsolutePath());
+					continue;
 				}
+				String textFilePath = new File(txtDir, sentenceId + ".txt").getAbsolutePath();
+				if (new File(textFilePath).exists()) {
+					// System.out.println("Skipping already existing file " + textFilePath);
+					continue;
+				}
+				System.out.println("Creating " + textFilePath);
+				FileUtils.createFileWithContents(textFilePath, text);
 			}
 		}
 	}
 
-	static HashMap<Integer, String> loadScript(File scriptFile) {
+	public static ArrayList<String> getScriptCategories(File scriptsDir, String suffix) {
+		File[] scriptFiles = scriptsDir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(suffix) && !name.startsWith(".");
+			}
+		});
+		if (scriptFiles == null) {
+			scriptFiles = new File[0];
+		}
+		ArrayList<String> categoriesList = new ArrayList<>();
+		for (File scriptFile : scriptFiles) {
+			String fileName = scriptFile.getName();
+			categoriesList.add(fileName.substring(0, fileName.lastIndexOf(suffix)));
+		}
+		if (categoriesList.size() > 1) {
+			Collections.sort(categoriesList);
+		}
+		return categoriesList;
+	}
+
+	static HashMap<String, String> loadScript(File scriptFile) {
 		System.out.println("Loading " + scriptFile.getAbsolutePath() + " ...");
-		HashMap<Integer, String> sentences = new HashMap<>();
+		HashMap<String, String> sentences = new HashMap<>();
 		try (BufferedReader br = new BufferedReader(new FileReader(scriptFile))) {
 			String line;
 			while ((line = br.readLine()) != null) {
 				String[] contents = line.split("\t");
-				if (contents.length != 2) {
+				if (contents.length < 2) {
 					System.out.println("Skipping line: " + line);
 					continue;
 				}
-				int lineNumber = Integer.parseInt(contents[0]);
-				sentences.put(lineNumber, contents[1]);
+				sentences.put(contents[0], contents[1]);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
