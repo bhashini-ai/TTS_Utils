d92e205f3dd392996abb238131ae939a5cb6c56b
Shiva Kumar H R <shivahr@gmail.com>
2024-07-15 18:54:30 +0530

    Create pruned transcripts file (i.e. after replacements)

--- src/main/java/ai/bhashini/tts/utils/CheckCharacters.java
+++ src/main/java/ai/bhashini/tts/utils/CheckCharacters.java
@@ -131,8 +131,7 @@ public class CheckCharacters {
 		return replacements;
 	}
 
-	void replace(String txtFilePath) {
-		String txt = FileUtils.getFileContents(txtFilePath).replaceAll("\n", " ").trim();
+	String replaceText(String txt) {
 		String newTxt = txt;
 		for (String oldStr : replacements.keySet()) {
 			String newStr = replacements.get(oldStr);
@@ -140,51 +139,77 @@ public class CheckCharacters {
 		}
 		// Replace pipe symbol with Devanagari danda
 		newTxt = newTxt.replaceAll("[|]", "।");
-		if (!newTxt.contentEquals(txt)) {
-			System.out.println(new File(txtFilePath).getName() + " -> " + txt + " -> " + newTxt);
-			FileUtils.createFileWithContents(txtFilePath, newTxt);
+		return newTxt;
+	}
+
+	void replaceAll(boolean verbose) {
+		for (String txtFilePath : txtFilePaths) {
+			String txt = FileUtils.getFileContents(txtFilePath).replaceAll("\n", " ").trim();
+			String newTxt = replaceText(txt);
+			if (!newTxt.contentEquals(txt)) {
+				if (verbose) {
+					System.out.println(new File(txtFilePath).getName() + " -> " + txt + " -> " + newTxt);
+				}
+				FileUtils.createFileWithContents(txtFilePath, newTxt);
+			}
 		}
 	}
 
-	void replaceAll() {
-		for (String txtFilePath : txtFilePaths) {
-			replace(txtFilePath);
+	void replace(File inFile, File outFile) {
+		try (BufferedReader br = new BufferedReader(new FileReader(inFile));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(outFile))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] contents = line.split("\t");
+				String id = contents[0];
+				String transcript = contents[1];
+				String newTranscript = replaceText(transcript);
+				bw.write(id + "\t" + newTranscript + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	public static class Arguments extends CommandLineOptions {
-		String defaultConcatenatedTextFilePath = "filelists/concatenatedText.txt";
+		String defaultConcatenatedTranscriptsFilePath = "filelists/concatenatedTranscripts.txt";
 		String defaultUniqueCharactersFilePath = "filelists/uniqueCharacters.txt";
 		String defaultReplacementsTSVFilePath = "filelists/replacements.tsv";
+		String defaultPrunedTranscriptsFilePath = "filelists/prunedTranscripts.txt";
 
 		StringOption dataDir = new StringOption("dir", "data-dir",
 				"Directory containing recordings (<child-dir>/wav/*.wav) and their transcripts (<child-dir>/txt/*.txt)");
-		StringOption concatenatedTextFilePath = new StringOption("ctfp", "concatenated-text-filepath",
-				"Relative path of text file containing concatenated-script " + "(default = '"
-						+ defaultConcatenatedTextFilePath + "')",
-				defaultConcatenatedTextFilePath);
+		StringOption concatenatedTranscriptsFilePath = new StringOption("ctfp", "concatenated-transcripts-filepath",
+				"Relative path of text file containing concatenated transcripts " + "(default = '"
+						+ defaultConcatenatedTranscriptsFilePath + "')",
+				defaultConcatenatedTranscriptsFilePath);
 		StringOption uniqueCharactersFilePath = new StringOption("ucfp", "unique-characters-filepath",
 				"Relative path of text file containing unique characters along with their unigram count "
 						+ "(default = '" + defaultUniqueCharactersFilePath + "')",
 				defaultUniqueCharactersFilePath);
 		StringOption replacementsTSVFilePath = new StringOption("rfp", "replacements-filepath",
-				"Relative path of text file containing the character replacements in TSV format " + "(default = '"
-						+ defaultReplacementsTSVFilePath + "')",
+				"Relative path of text file containing the character replacements to be done (in TSV format) "
+						+ "(default = '" + defaultReplacementsTSVFilePath + "')",
 				defaultReplacementsTSVFilePath);
+		StringOption prunedTranscriptsFilePath = new StringOption("out", "pruned-transcripts-filepath",
+				"Relative path of file to which pruned transcripts (i.e. after replacements) have to be saved "
+						+ "(default = '" + defaultPrunedTranscriptsFilePath + "')",
+				defaultPrunedTranscriptsFilePath);
 		BooleanOption concatenateAgain = new BooleanOption("recreate", "concatenate-again",
 				"Force recreation of concatenated-text and unique-characters files");
-		BooleanOption skipReplacements = new BooleanOption("skip", "skip-replacements", "Skip character replacements");
+		BooleanOption inplaceReplacements = new BooleanOption("inplace", "inplace-replacements",
+				"Do character replacements in the original transcript files");
 		BooleanOption verbose = new BooleanOption("v", "verbose", "Print each file being processed");
 
 		public Arguments() {
 			super();
 			dataDir.setRequired(true);
 			options.addOption(dataDir);
-			options.addOption(concatenatedTextFilePath);
+			options.addOption(concatenatedTranscriptsFilePath);
 			options.addOption(uniqueCharactersFilePath);
 			options.addOption(replacementsTSVFilePath);
 			options.addOption(concatenateAgain);
-			options.addOption(skipReplacements);
+			options.addOption(inplaceReplacements);
 			options.addOption(verbose);
 		}
 	}
@@ -200,17 +225,19 @@ public class CheckCharacters {
 			return;
 		}
 		String dataDirPath = arguments.dataDir.getStringValue();
-		String concatenatedTextFilePath = arguments.concatenatedTextFilePath.getStringValue();
+		String concatenatedTranscriptsFilePath = arguments.concatenatedTranscriptsFilePath.getStringValue();
 		String uniqueCharactersFilePath = arguments.uniqueCharactersFilePath.getStringValue();
 		String replacementsTSVFilePath = arguments.replacementsTSVFilePath.getStringValue();
+		String prunedTranscriptsFilePath = arguments.prunedTranscriptsFilePath.getStringValue();
 		boolean concatenateAgain = arguments.concatenateAgain.getBoolValue();
-		boolean skipReplacements = arguments.skipReplacements.getBoolValue();
+		boolean inplaceReplacements = arguments.inplaceReplacements.getBoolValue();
 		boolean verbose = arguments.verbose.getBoolValue();
 
 		File dataDir = new File(dataDirPath);
-		File concatenatedTxtFile = new File(dataDir, concatenatedTextFilePath);
+		File concatenatedTxtFile = new File(dataDir, concatenatedTranscriptsFilePath);
 		File uniqueCharactersFile = new File(dataDir, uniqueCharactersFilePath);
 		File replacementsTSVFile = new File(dataDir, replacementsTSVFilePath);
+		File prunedTranscriptsFile = new File(dataDir, prunedTranscriptsFilePath);
 
 		CheckCharacters checkCharacters = new CheckCharacters();
 		if (!concatenatedTxtFile.exists() || concatenateAgain) {
@@ -221,19 +248,22 @@ public class CheckCharacters {
 			System.out.println("Successfully completed concatenation.");
 		}
 		if (!uniqueCharactersFile.exists() || concatenateAgain) {
-			System.out.println(
-					"Finding unique characters and their unigram count from " + concatenatedTxtFile.getAbsolutePath());
+			System.out.println("Finding unique characters and their unigram count from " + concatenatedTxtFile.getAbsolutePath());
 			checkCharacters.createUniqueCharactersFile(concatenatedTxtFile, uniqueCharactersFile);
 			System.out.println("Saved unique characters and their count to " + uniqueCharactersFile.getAbsolutePath());
 		}
-		if (!skipReplacements) {
-			if (!replacementsTSVFile.exists()) {
-				System.out.println("Error: Could not find the TSV file containing replacements "
-						+ replacementsTSVFile.getAbsolutePath());
-				return;
-			}
-			checkCharacters.loadReplacements(replacementsTSVFile);
-			checkCharacters.replaceAll();
+		if (!replacementsTSVFile.exists()) {
+			System.out.println("Error: Could not find the TSV file containing replacements "
+					+ replacementsTSVFile.getAbsolutePath());
+			return;
+		}
+		checkCharacters.loadReplacements(replacementsTSVFile);
+		if (inplaceReplacements) {
+			checkCharacters.replaceAll(verbose);
+		} else {
+			System.out.println("Replacing specified characters in " + concatenatedTxtFile.getAbsolutePath());
+			checkCharacters.replace(concatenatedTxtFile, prunedTranscriptsFile);
+			System.out.println("Saved pruned transcripts (after replacements) to " + prunedTranscriptsFile.getAbsolutePath());
 		}
 	}
 
