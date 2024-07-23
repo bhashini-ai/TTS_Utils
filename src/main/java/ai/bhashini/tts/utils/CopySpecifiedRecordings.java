62e91b604f8b3c41f4aa16d6561b6f2ea7249dad
Shiva Kumar H R <shivahr@gmail.com>
2024-07-23 16:18:25 +0530

    Refactor to improve code readability.

--- src/main/java/ai/bhashini/tts/utils/CopySpecifiedRecordings.java
+++ src/main/java/ai/bhashini/tts/utils/CopySpecifiedRecordings.java
@@ -14,6 +14,78 @@ import java.util.HashSet;
 import org.apache.commons.cli.ParseException;
 
 public class CopySpecifiedRecordings {
+	public static ArrayList<String> loadSentenceIds(File sentenceIdsFile) {
+		ArrayList<String> sentenceIds = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(sentenceIdsFile))) {
+			String line = br.readLine(); // ignore header
+			while ((line = br.readLine()) != null) {
+				String sentenceId = line.split("\t")[0];
+				sentenceIds.add(sentenceId);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sentenceIds;
+	}
+
+	public static void loadTxtAndWavFilePaths(File dataDir, ArrayList<String> sentenceIds, String wavDirName,
+			HashMap<String, String> txtFilePaths, HashMap<String, String> wavFilePaths) {
+		HashSet<String> sentenceIdsMap = new HashSet<String>(sentenceIds);
+		File[] subDirs = MatchWavAndTextFiles.getSubDirs(dataDir);
+		for (File subDir : subDirs) {
+			File txtDir = new File(subDir, "txt");
+			if (txtDir.exists()) {
+				loadFilePaths(txtDir, sentenceIdsMap, txtFilePaths, "txt");
+			}
+			File wavDir = new File(subDir, wavDirName);
+			if (wavDir.exists()) {
+				loadFilePaths(wavDir, sentenceIdsMap, wavFilePaths, wavDirName);
+			}
+		}
+	}
+
+	public static void loadFilePaths(File baseDir, HashSet<String> sentenceIdsMap, HashMap<String, String> filePaths,
+			String extension) {
+		File[] files = baseDir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				String nameWithoutExtension = FileUtils.getFileNameWithoutExtension(name);
+				return sentenceIdsMap.contains(nameWithoutExtension);
+			}
+		});
+		for (File file : files) {
+			String sentenceId = FileUtils.getFileNameWithoutExtension(file.getName());
+			filePaths.put(sentenceId, file.getAbsolutePath());
+		}
+	}
+
+	public static void copyTextAndWavFiles(ArrayList<String> sentenceIds, HashMap<String, String> txtFilePaths,
+			HashMap<String, String> wavFilePaths, File outputTxtDir, File outputWavDir) {
+		for (String sentenceId : sentenceIds) {
+			if (txtFilePaths.containsKey(sentenceId) && wavFilePaths.containsKey(sentenceId)) {
+				try {
+					File srcTxtFile = new File(txtFilePaths.get(sentenceId));
+					File dstTxtFile = new File(outputTxtDir, sentenceId + ".txt");
+					Files.copy(srcTxtFile.toPath(), dstTxtFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+					System.out.println("Copied " + srcTxtFile + " -> " + dstTxtFile);
+
+					File srcWavFile = new File(wavFilePaths.get(sentenceId));
+					File dstWavFile = new File(outputWavDir, sentenceId + ".wav");
+					Files.copy(srcWavFile.toPath(), dstWavFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+					System.out.println("Copied " + srcWavFile + " -> " + dstWavFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				if (!txtFilePaths.containsKey(sentenceId)) {
+					System.out.println("Missing txt file for " + sentenceId);
+				}
+				if (!wavFilePaths.containsKey(sentenceId)) {
+					System.out.println("Missing wav file for " + sentenceId);
+				}
+			}
+		}
+	}
 
 	public static class Arguments extends CommandLineOptions {
 		String defaultOutputDirName = "temp";
@@ -54,82 +126,20 @@ public class CopySpecifiedRecordings {
 		File sentenceIdsFile = new File(dataDir, tsvFilePath);
 		File outputDir = new File(dataDir, outputDirName);
 
-		File outputTxtDir = new File(outputDir, "txt");
-		File outputWavDir = new File(outputDir, "wav");
-		outputTxtDir.mkdirs();
-		outputWavDir.mkdirs();
 		System.out.println("Parsing sentenceIds in " + sentenceIdsFile.getAbsolutePath());
-		ArrayList<String> sentenceIds = new ArrayList<>();
-		try (BufferedReader br = new BufferedReader(new FileReader(sentenceIdsFile))) {
-			String line = br.readLine(); // ignore header
-			while ((line = br.readLine()) != null) {
-				String sentenceId = line.split("\t")[0];
-				sentenceIds.add(sentenceId);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		ArrayList<String> sentenceIds = loadSentenceIds(sentenceIdsFile);
+
 		HashMap<String, String> txtFilePaths = new HashMap<String, String>();
 		HashMap<String, String> wavFilePaths = new HashMap<String, String>();
 		System.out.println("Parsing txt and wav files in " + dataDir.getAbsolutePath());
 		loadTxtAndWavFilePaths(dataDir, sentenceIds, "wav", txtFilePaths, wavFilePaths);
+
+		File outputTxtDir = new File(outputDir, "txt");
+		File outputWavDir = new File(outputDir, "wav");
+		outputTxtDir.mkdirs();
+		outputWavDir.mkdirs();
 		System.out.println("# of matching txt files found = " + txtFilePaths.size());
 		System.out.println("# of matching wav files found = " + wavFilePaths.size() + "\n");
-		for (String sentenceId: sentenceIds) {
-			if (txtFilePaths.containsKey(sentenceId) && wavFilePaths.containsKey(sentenceId)) {
-				try {
-					File srcTxtFile = new File(txtFilePaths.get(sentenceId));
-					File dstTxtFile = new File(outputTxtDir, sentenceId + ".txt");
-					Files.copy(srcTxtFile.toPath(), dstTxtFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-					System.out.println("Copied " + srcTxtFile + " -> " + dstTxtFile);
-
-					File srcWavFile = new File(wavFilePaths.get(sentenceId));
-					File dstWavFile = new File(outputWavDir, sentenceId + ".wav");
-					Files.copy(srcWavFile.toPath(), dstWavFile.toPath(),  StandardCopyOption.REPLACE_EXISTING);
-					System.out.println("Copied " + srcWavFile + " -> " + dstWavFile);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} else {
-				if (!txtFilePaths.containsKey(sentenceId)) {
-					System.out.println("Missing txt file for " + sentenceId);
-				}
-				if (!wavFilePaths.containsKey(sentenceId)) {
-					System.out.println("Missing wav file for " + sentenceId);
-				}
-			}
-		}
+		copyTextAndWavFiles(sentenceIds, txtFilePaths, wavFilePaths, outputTxtDir, outputWavDir);
 	}
-
-	public static void loadTxtAndWavFilePaths(File dataDir, ArrayList<String> sentenceIds, String wavDirName,
-			HashMap<String, String> txtFilePaths, HashMap<String, String> wavFilePaths) {
-		HashSet<String> sentenceIdsMap = new HashSet<String>(sentenceIds);
-		File[] subDirs = MatchWavAndTextFiles.getSubDirs(dataDir);
-		for (File subDir : subDirs) {
-			File txtDir = new File(subDir, "txt");
-			if (txtDir.exists()) {
-				loadFilePaths(txtDir, sentenceIdsMap, txtFilePaths, "txt");
-			}
-			File wavDir = new File(subDir, wavDirName);
-			if (wavDir.exists()) {
-				loadFilePaths(wavDir, sentenceIdsMap, wavFilePaths, wavDirName);
-			}
-		}
-	}
-
-	public static void loadFilePaths(File baseDir, HashSet<String> sentenceIdsMap, HashMap<String, String> filePaths,
-			String extension) {
-		File[] files = baseDir.listFiles(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				String nameWithoutExtension = FileUtils.getFileNameWithoutExtension(name);
-				return sentenceIdsMap.contains(nameWithoutExtension);
-			}
-		});
-		for (File file : files) {
-			String sentenceId = FileUtils.getFileNameWithoutExtension(file.getName());
-			filePaths.put(sentenceId, file.getAbsolutePath());
-		}
-	}
-
 }
