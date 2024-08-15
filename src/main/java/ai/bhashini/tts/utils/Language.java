f8e4b941733a2a0d68b282fba6852d1cd4d60fb5
Shiva Kumar H R <shivahr@gmail.com>
2024-08-15 11:24:25 +0530

    Add ISO-15924 code for script and BCP-47 tag for language

--- src/main/java/ai/bhashini/tts/utils/Language.java
+++ src/main/java/ai/bhashini/tts/utils/Language.java
@@ -12,7 +12,7 @@ public enum Language {
 	Urdu(Script.Urdu, "urd", "ur"),
 	Bhojpuri(Script.Devanagari, "bho", "bho"), // https://en.wikipedia.org/wiki/Bhojpuri_language
 	Kannada(Script.Kannada, "kan", "kn"),
-	Oriya(Script.Oriya, "ori", "or"),
+	Odia(Script.Odia, "ori", "or", "ory_Orya"),
 	Malayalam(Script.Malayalam, "mal", "ml"),
 	Punjabi(Script.Gurmukhi, "pan", "pa"),
 	Chhattisgarhi(Script.Devanagari, "hne", "hne"), // https://en.wikipedia.org/wiki/Chhattisgarhi_language
@@ -21,22 +21,28 @@ public enum Language {
 	Magahi(Script.Devanagari, "mag", "mag"), // https://en.wikipedia.org/wiki/Magahi_language
 	Santali(Script.Devanagari, "sat", "sat"), // https://en.wikipedia.org/wiki/Santali_language
 	Kashmiri(Script.Devanagari, "kas", "ks"),
-	Nepali(Script.Devanagari, "nep", "ne"),
+	Nepali(Script.Devanagari, "nep", "ne", "npi_Deva"),
 	Sindhi(Script.Devanagari, "snd", "sd"),
 	Dogri(Script.Devanagari, "doi", "doi"), // https://en.wikipedia.org/wiki/Dogri_language
-	Konkani(Script.Devanagari, "kok", "kok"), // https://en.wikipedia.org/wiki/Konkani_language
+	Konkani(Script.Devanagari, "kok", "kok", "gom_Deva"), // https://en.wikipedia.org/wiki/Konkani_language
 	Manipuri(Script.Bengali, "mni", "mni"), // https://en.wikipedia.org/wiki/Meitei_language
 	Bodo(Script.Devanagari, "brx", "brx"), // https://en.wikipedia.org/wiki/Boro_language_(India)
 	Sanskrit(Script.Devanagari, "san", "sa");
 
-	private Language(Script script, String threeLetterCode, String twoLetterCode) {
+	private Language(Script script, String threeLetterCode, String twoLetterCode, String bcp47Tag) {
 		this.script = script;
 		this.threeLetterCode = threeLetterCode;
 		this.twoLetterCode = twoLetterCode;
+		this.bcp47Tag = bcp47Tag;
+	}
+
+	private Language(Script script, String threeLetterCode, String twoLetterCode) {
+		this(script, threeLetterCode, twoLetterCode, threeLetterCode + "_" + script.fourLetterCode);
 	}
 
 	public final Script script;
 	public final String threeLetterCode;
 	public final String twoLetterCode;
+	public final String bcp47Tag;
 
 }
--- src/main/java/ai/bhashini/tts/utils/Script.java
+++ src/main/java/ai/bhashini/tts/utils/Script.java
@@ -1,32 +1,34 @@
 package ai.bhashini.tts.utils;
 
 public enum Script {
-	Devanagari(0x0900),
-	Bengali(0x0980),
-	Gurmukhi(0x0A00),
-	Gujarati(0x0A80),
-	Oriya(0x0B00),
-	Tamil(0x0B80),
-	Telugu(0x0C00),
-	Kannada(0x0C80),
-	Malayalam(0x0D00),
-	English(0x0000, 0x007F, 0x0030, 0x0039),
-	Urdu(0x0600, 0x06FF, 0x06F0, 0x06F9);
+	Devanagari(0x0900, "Deva"),
+	Bengali(0x0980, "Beng"),
+	Gurmukhi(0x0A00, "Guru"),
+	Gujarati(0x0A80, "Gujr"),
+	Odia(0x0B00, "Orya"),
+	Tamil(0x0B80, "Taml"),
+	Telugu(0x0C00, "Telu"),
+	Kannada(0x0C80, "Knda"),
+	Malayalam(0x0D00, "Mlym"),
+	English(0x0000, 0x007F, 0x0030, 0x0039, "Latn"),
+	Urdu(0x0600, 0x06FF, 0x06F0, 0x06F9, "Arab");
 
 	public final int unicodeBlockStart;
 	public final int unicodeBlockEnd;
 	public final int digitZero;
 	public final int digitNine;
+	public final String fourLetterCode;
 
-	private Script(int unicodeBlockStart) {
-		this(unicodeBlockStart, unicodeBlockStart + 0x7F, unicodeBlockStart + 0x66, unicodeBlockStart + 0x6F);
+	private Script(int unicodeBlockStart, String fourLetterCode) {
+		this(unicodeBlockStart, unicodeBlockStart + 0x7F, unicodeBlockStart + 0x66, unicodeBlockStart + 0x6F, fourLetterCode);
 	}
 
-	private Script(int unicodeBlockStart, int unicodeBlockEnd, int digitZero, int digitNine) {
+	private Script(int unicodeBlockStart, int unicodeBlockEnd, int digitZero, int digitNine, String fourLetterCode) {
 		this.unicodeBlockStart = unicodeBlockStart;
 		this.unicodeBlockEnd = unicodeBlockEnd;
 		this.digitZero = digitZero;
 		this.digitNine = digitNine;
+		this.fourLetterCode = fourLetterCode;
 	}
 
 	public boolean isValidCodepoint(int c) {
